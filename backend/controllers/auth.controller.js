b405e4b50a8dbc2559d633c99e775476b1d4a4b6
Arvind-iitr <pheonixblade001@gmail.com>
2025-04-18 20:54:40 +0530

    login functionality is addded to the page

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -5,6 +5,7 @@ import User from "../models/users.model.js"
 import { transporter } from '../utils/nodemailer.js';
 import { generateWelcomeEmail } from '../utils/nodemailer.js';
 
+//signup route to save new user, gen token and send welcome email
 export const signup = async (req, res) => {
     try {
         const { username, email, password } = req.body;
@@ -29,10 +30,10 @@ export const signup = async (req, res) => {
             from: process.env.EMAIL,
             to: email,
             subject: 'Welcome to our Pokedex!',
-            html : generateWelcomeEmail(username)
+            html : generateWelcomeEmail(username) //define the email content with the username
         }
 
-        await transporter.sendMail(emailObject)
+        await transporter.sendMail(emailObject)//sending the email via nodemailer
 
         res.status(201).json({ message: 'User created successfully'  });
         
@@ -41,4 +42,37 @@ export const signup = async (req, res) => {
     }
 }
 
+//login route to authenticate user and gen token
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email }); //find the user in database 
+
+        if(!user){
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password); // compare the hashed password with the password
+
+        if(!isPasswordMatch){
+            return res.status(400).json({ message: 'Invalid password' });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: '3d' }); // Generate a JWT token for the user using id and expiration time of 3 days
+
+        res.cookie('token' , token , {
+            httpOnly: true,
+            secure: process.env.MODE === 'production', // Set secure flag to true when running in production mode
+            sameSite: process.env.MODE === 'production'? 'none' : 'strict', // Set SameSite to 'none' when running in production mode or 'strict' when running in development mode
+            maxAge: 3 * 24 * 60 * 60 * 1000, // Expire in 3 days
+            
+        })
+
+        res.json({ message: 'User logged in successfully' });
+
+
+    } catch (error) {
+        res.json({ message: error.message });
+    }
+}
 
